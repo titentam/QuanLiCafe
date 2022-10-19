a7b4e842d9e1024e07a7786e81072ad4f8c26b7a
duypro432003 <97208783+duypro432003@users.noreply.github.com>
2022-10-19 22:47:36 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,37 +10,15 @@
 using namespace std;
 
 int main() {
-	DuLieu data;
+DuLieu data;
 	data.docFileDsDoUong();
 	data.docFileDsNhanVien();
-	//data.xuatFileDsDoUong(cout);
-	cout << endl;
-
-	Vector<DoUong> &menu = data.getDoUong();
+	Vector<DoUong> menu = data.getDoUong();
 	
-	NhanVienPhucVu tam = (data.getNvPhucVu())[0];
-	Vector<DoUong> dsdouong;
-	Vector<int> sl;
-	// 12
-	// tim do uong co ma 12 -> them dsdouong , them -> soluong
-	dsdouong.push_back(menu[2]);
-	sl.push_back(70);
-
-	dsdouong.push_back(menu[3]);
-	sl.push_back(80);
-
-	dsdouong.push_back(menu[5]);
-	sl.push_back(50);
-
-	tam.lamDoUong(data, dsdouong, sl);
-	tam.xuatBill(cout, dsdouong, sl);
-	//data.xuatFileDsDoUong(cout);
-	/*ofstream f;
-	string t = "TAM\\ti\\tamtentoi.txt";
-	f.open(t, ios::out);
-	tam.xuatBill(f, dsdouong, sl);
-	f.close();*/
-
+	NhanVienPhucVu nvpv = (data.getNvPhucVu())[0];
+	NhanVienQuanLi nvql = (data.getNvQuanLi())[0];
+	//nvql.menuQuanLi(data);
+	nvpv.menuPhucvu(data);
 	
 
 	return 0;
