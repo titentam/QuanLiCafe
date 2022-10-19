1f4e6a9879d6dc1413a10d418bc3c303f2522de8
Tâm Phan <phantam04012003@gmail.com>
2022-10-19 11:01:00 +0700

    menu

--- main.cpp
+++ main.cpp
@@ -118,36 +118,13 @@ void nhapmk() {
 	cout << endl << res;
 }
 int main() {
-	//DuLieu data;
-	//data.docFileDsDoUong();
-	//data.docFileDsNhanVien();
-	//
+	DuLieu data;
+	data.docFileDsDoUong();
+	data.docFileDsNhanVien();
 
-	//NhanVienQuanLi nvql;
-	//nvql = (data.getNvQuanLi())[0];
+	NhanVienPhucVu nvpv = (data.getNvPhucVu())[0];
+	nvpv.xuatThongTin();
 
-	//nvql.xemDsNV(data);
-	///*
-	//nvql.themNV(data, { "01asdad","Tam Ten Toi","8123712738127","tamcute","12312h",2 });
-	//cout << endl;
-	//nvql.xemDsNV(data);*/
-	//
-	///*nvql.xemDsDoUong(data);
-	//DoUong ncdua(19, "Nuoc dua", 10000, 100, "Nuoc trai cay");
-	//nvql.themMon(data, ncdua);
-	//cout << endl;
-	//nvql.xemDsDoUong(data);*/
-	//nvql.xemDsDoUong(data);
-	//cout << endl;
-	//nvql.xoaMon(data, 12);
-	//cout << endl;
-	//nvql.xemDsDoUong(data);
-	wellcome();
-	loadingbar();
-	main_menu();
-	menu_quanli();
-	menu_phucvu();
-	nhapmk();
 	
 	return 0;
 }
